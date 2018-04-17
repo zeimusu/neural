05ceb0c028662cf0b3e251a7fc41c3e7c9cdaaf9
James Kilfiger <james.kilfiger@gmail.com>
2018-04-17 17:34:46 +0100

    create files from weight tensor

---
+++ weightednetwork.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func makeWeightLayer(weights [][]float64, prevLayerSize int) (layer, error) {
+	l := make(layer, len(weights))
+	for i := range l {
+		if len(weights[i]) != prevLayerSize {
+			return l, fmt.Errorf("Mismatch on length of weight vector, expect %v, got %v", prevLayerSize, len(weights[i]))
+		}
+		l[i] = MakeWeightedAvNeuron(weights[i])
+	}
+	return l, nil
+}
+
+/*
+Each neuron has weights for each input: two inputs, 4, 4 ,1
+weights =[
+ [..]   [....]   [[....]]
+[[..]],[[....]],
+ [..]   [....]
+ [..]   [....]
+ ]
+
+ len(weights)= number of hidden+output layers
+ len(weights[0] = number of neurons in layer 1)
+ weights[0][i] = weights for ith neuron in layer 1
+
+*/
+
+func MakeWeightedNetwork(numInputs int, weights [][][]float64) network {
+	var err error
+	n := make(network, len(weights)+1)
+	n[0] = make(layer, numInputs)
+	n.InitInput()
+	previousLayerSize := numInputs
+	for i := 0; i < len(n); i++ {
+		n[i], err = makeWeightLayer(weights[i], previousLayerSize)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return n
+}
