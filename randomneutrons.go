d0a700332bc6b6250d7cfcbf6fa3cf4f2e5d1e1e
James Kilfiger <james.kilfiger@gmail.com>
2018-04-18 23:49:46 +0100

    testing for random sigmoid

--- randomneutrons.go
+++ randomneutrons.go
@@ -6,6 +6,7 @@ func init() {
 	rand.Seed(100)
 }
 
+//return a randomlist of given length
 func randList(length int, a, b float64) []float64 {
 	out := make([]float64, length)
 	for i := range out {
@@ -14,6 +15,7 @@ func randList(length int, a, b float64) []float64 {
 	return out
 }
 
+//create random weights for one layer
 func randLayerWeights(numNeurons, numInputs int) [][]float64 {
 	out := make([][]float64, numNeurons)
 	for i := range out {
@@ -22,6 +24,7 @@ func randLayerWeights(numNeurons, numInputs int) [][]float64 {
 	return out
 }
 
+//create the random weights for a whole network
 func randNetworkWeights(layerSizes []int) [][][]float64 {
 	out := make([][][]float64, len(layerSizes)-1)
 	for i := 1; i < len(layerSizes); i++ {
@@ -30,11 +33,13 @@ func randNetworkWeights(layerSizes []int) [][][]float64 {
 	return out
 }
 
+//MakeRandomSumNetwork Creates a random network
 func MakeRandomSumNetwork(layerSizes []int) (Network, [][][]float64) {
 	weights := randNetworkWeights(layerSizes)
 	return MakeWeightedNetwork(layerSizes[0], weights), weights
 }
 
+//Make RandomSigmoid makes a random network in whihc all the biass are zero and the weights are random
 func MakeRandomSigmoid(layerSizes []int) (Network, [][][]float64, [][]float64) {
 	weights := randNetworkWeights(layerSizes)
 	biases := make([][]float64, len(layerSizes)-1)
