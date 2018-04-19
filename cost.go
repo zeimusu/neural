6b77d2d46a49c74e34475d49998842cd3ef5e307
James Kilfiger <james.kilfiger@gmail.com>
2018-04-19 09:27:38 +0100

    cost function

---
+++ cost.go
@@ -0,0 +1,24 @@
+package neural
+
+import "log"
+
+func cost(outputs, desired []float64) float64 {
+	if len(outputs) != len(desired) {
+		log.Fatal("Desired Length doesn't match output length")
+	}
+	total := 0.
+	for i := range outputs {
+		total += (outputs[i] - desired[i]) * (outputs[i] - desired[i])
+	}
+	return total
+}
+
+func TotalCost(net Network, inputs, desired [][]float64) float64 {
+	c := 0.
+	for i, input := range inputs {
+		net.SetInput(input)
+		output := net.GetOutputValues()
+		c += cost(output, desired[i])
+	}
+	return c / float64(2*len(inputs))
+}
